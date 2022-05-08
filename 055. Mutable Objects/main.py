1f31db0b25a05230ce4b880fa574830661cf2525
Mariia Gorbachova <vg.shamking@gmail.com>
2022-05-08 11:57:57 +0200

    2022-05-08

---
+++ 055. Mutable Objects/main.py
@@ -0,0 +1,14 @@
+shopping_list = ["milk",
+                 "pasta",
+                 "eggs",
+                 "spam",
+                 "bread",
+                 "rice"
+                 ]
+another_list = shopping_list
+print(id(shopping_list))
+print(id(another_list))
+
+shopping_list += ["cookies"]
+print(shopping_list)
+print(id(shopping_list))
