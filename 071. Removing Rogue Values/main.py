e5b7d272c2ad2cc191c6017b6742f39def6ae024
Mariia Gorbachova <vg.shamking@gmail.com>
2022-05-15 09:21:27 +0200

    2022-05-15

---
+++ 071. Removing Rogue Values/main.py
@@ -0,0 +1,12 @@
+data = [104, 101, 4, 105, 308, 103, 5,
+        107, 100, 306,106, 102, 108]
+min_value = 100
+max_value = 200
+
+for index in range(len(data) - 1, - 1, -1):
+        if data[index] < min_value or data[index] > max_value:
+            print(index, data)
+            del data[index]
+
+print(data)
+
