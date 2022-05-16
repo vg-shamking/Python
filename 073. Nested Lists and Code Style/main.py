e0382e24924e4925b85c8d8ea99fd11ae3d64f9c
Mariia Gorbachova <vg.shamking@gmail.com>
2022-05-16 19:47:29 +0200

    2022-05-16

---
+++ 073. Nested Lists and Code Style/main.py
@@ -0,0 +1,33 @@
+empty_list = []
+even = [2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+
+numbers = [even, odd]
+print(numbers)
+
+for numbers_list in numbers:
+    print(numbers_list)
+
+    for value in numbers_list:
+        print(value)
+
+menu = [
+    ["egg", "bacon"],
+    ["egg", "sausage", "bacon"],
+    ["egg", "spam"],
+    ["egg", "bacon", "spam"],
+    ["egg", "bacon", "sausage", "spam"],
+    ["spam", "bacon", "sausage", "spam"],
+    ["spam", "sausage", "spam", "bacon", "spam", "tomato", "spam"],
+    ["spam", "egg", "spam", "spam", "bacon", "spam"]
+]
+
+for meal in menu:
+    if "spam" not in meal:
+        print(meal)
+
+        for item in meal:
+            print(item)
+    else:
+        print("{0} has a spam score of {1}"
+              .format(meal, meal.count("spam")))
---
+++ 074. Function Signature/moreprint.py
@@ -0,0 +1,9 @@
+# Built-in Functions
+# print has signatures (*objects, sep='', end='\n', file=sys.stdout, flush=False)
+name = "Tim"
+age = 10
+
+print(name, age, "Python", 2020)
+print(name, age, "Python", 2020, sep=", ")  # Adding separators
+
+
