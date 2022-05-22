3180aeeecfbe76f3d774fb4800072d583200b205
Mariia Gorbachova <vg.shamking@gmail.com>
2022-05-22 17:38:29 +0200

    2022-05-22

---
+++ 080. Practical Usage of Unpacking Tuples/main.py
@@ -0,0 +1,19 @@
+for t in enumerate("abcdefgh"):
+    index, character = t
+    print(index, character)
+
+# index, character = (0, 'a')
+# print(index)
+# print(character)
+
+# output:
+# (0, 'a')
+# (1, 'b')
+# (2, 'c')
+# (3, 'd')
+# (4, 'e')
+# (5, 'f')
+# (6, 'g')
+# (7, 'h')
+# 0
+# a
---
+++ 081. More Unpacking/main.py
@@ -0,0 +1,17 @@
+welcome = "Welcome to my Nighrmare", "Alice Cooper", 1975
+bad = "Bad Company", "Bad Company", 1974
+budgie = "Nightflight", "Budgie", 1981
+imelda = "More Mayhem", "Emilda May", 2011
+metallica = "Ride the Lightning", "Metallica", 1984
+
+title, artist, year = metallica
+print(title)
+print(artist)
+print(year)
+
+table = ("Coffee table", 200, 100, 75, 34.50)
+print(table[1] * table[2])
+
+# Unpacking
+name, length, width, height, price = table
+print(length * width)
