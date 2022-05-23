1efcb1b23edd1f5d99cf11ca2b0eb0eddc0dabca
Mariia Gorbachova <vg.shamking@gmail.com>
2022-05-23 19:31:18 +0200

    2022-05-23

---
+++ 083. Nesting Further/main.py
@@ -0,0 +1,40 @@
+# albums = [("Welcome to my Nighrmare", "Alice Cooper", 1975),
+#           ("Bad Company", "Bad Company", 1974),
+#           ("Nightflight", "Budgie", 1981),
+#           ("More Mayhem", "Emilda May", 2011),
+#           ("Ride the Lightning", "Metallica", 1984),
+#           ]
+#
+# print(len(albums))
+#
+# for name, artist, year in albums:
+#     print("Album: {}, Artist: {}, Year: {}"
+#           .format(name, artist, year))
+# print()
+#
+# for album in albums:  # another variant
+#     name, artist, year = album
+#     print("Album: {}, Artist: {}, Year: {}"
+#           .format(name, artist, year))
+#
+#
+#
+#
+#
+# Tuple containing a list of tuples:
+# ("More Mayhem", "Imelda May", 2011,
+# [
+#     (1, "Pulling the Rug"),
+#     (2, "Psyco"),
+#     (3, "Mayhem")
+# ]
+# )
+
+# Tuple containing tuple of tuples:
+# ("More Mayhem", "Imelda May", 2011,
+# (
+#     (1, "Pulling the Rug"),
+#     (2, "Psyco"),
+#     (3, "Mayhem")
+# )
+# )
