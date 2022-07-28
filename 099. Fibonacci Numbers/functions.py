3f79db5e4237b33388a7e8617bbc03ab85e34c7c
Mariia Gorbachova <vg.shamking@gmail.com>
2022-07-28 16:46:41 +0200

    2022-07-28

---
+++ 099. Fibonacci Numbers/functions.py
@@ -0,0 +1,42 @@
+def multiply(x, y):
+    result = x * y
+    return result
+
+
+def is_palindrome(string):
+    # backwards = string[::-1]
+    # return backwards == string
+    return string[::-1].casefold() == string.casefold()
+
+
+def palindrome_sentence(sentence):
+    string = ""
+    for char in sentence:
+        if char.isalnum():
+            string += char
+
+    print(string)
+    # return string[::-1].casefold() == string.casefold()
+    return is_palindrome(string)
+
+
+def fibonacci(n):
+    """
+    Return the `n` th Fibonacci number, for positive `n` .
+    """
+    if 0 <= n <= 1:
+        return n
+
+    n_minus1, n_minus2 = 1, 0
+
+    result = None
+    for f in range(n - 1):
+        result = n_minus2 + n_minus1
+        n_minus2 = n_minus1
+        n_minus1 = result
+
+    return result
+
+
+for i in range(36):
+    print(i, fibonacci(i))
