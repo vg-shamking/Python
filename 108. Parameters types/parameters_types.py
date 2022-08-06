dc31b7a32e7a77ccd62ce95b4e73ae354bb4aa9d
Mariia Gorbachova <vg.shamking@gmail.com>
2022-08-06 03:16:48 +0200

    2022-08-06

---
+++ 108. Parameters types/parameters_types.py
@@ -0,0 +1,10 @@
+#  We're investigation only types of parameters by this program.
+
+def func(p1, p2, *args, k, **kwargs):
+    print("positional-or-keyword:...{}, {}".format(p1, p2))
+    print("var-positional (*args):..{}".format(args))
+    print("keyword:.................{}".format(k))
+    print("var-keyword:.............{}".format(kwargs))
+
+
+func(1, 2, 3, 4, 5, 9, k=6, key1=7, key2=8)
