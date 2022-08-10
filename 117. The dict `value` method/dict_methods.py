c13d5728428b6d4fe21af5c0a101d0498b7f5dfb
Mariia Gorbachova <vg.shamking@gmail.com>
2022-08-11 00:15:20 +0200

    2022-08-11

---
+++ 117. The dict `value` method/dict_methods.py
@@ -0,0 +1,37 @@
+d = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    "iv": "four",
+}
+
+pantry_items = ['chicken', 'spam', 'egg', 'bread', 'lemon']
+
+v = d.values()
+print(v)
+
+d[10] = "ten"
+print(v)
+
+print("four" in v)
+print("eleven" in v)
+
+keys = list(d.keys())
+values = list(v)  # => list(d.values())
+if "four" in values:
+    index = values.index("four")
+    key = keys[index]
+    print(f"{d[key]} was found with the key {key}")
+
+print()
+
+for key, value in d.items():
+    if value == "four":
+        print(f"{d[key]} was found with the key {key}")
