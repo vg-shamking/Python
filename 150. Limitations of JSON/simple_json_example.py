4e0753d6bee5cf6da809fd5b1dc7ed934b2d1a77
Mariia Gorbachova <vg.shamking@gmail.com>
2022-09-09 02:16:59 +0200

    2022-09-09

---
+++ 150. Limitations of JSON/simple_json_example.py
@@ -0,0 +1,18 @@
+import json
+
+# JSON not supports TUPLES - only LISTS
+languages = [
+    ('ABC', 1987),
+    ('Algol 68', 1968),
+    ('APL', 1962),
+    ('C', 1973),
+    ('Haskell', 1990),
+    ('Lisp', 1958),
+    ('Modula-2', 1977),
+    ('Perl', 1987),
+]
+
+with open('test.json', 'r', encoding='utf-8') as testfile:
+    data = json.load(testfile)
+print(data)
+print(data[2])
