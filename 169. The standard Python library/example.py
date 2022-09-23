d6273bda477740d01d4fd4e2f3d318e6a7a24ff4
Mariia Gorbachova <vg.shamking@gmail.com>
2022-09-24 00:18:52 +0200

    2022-09-24

---
+++ 169. The standard Python library/example.py
@@ -0,0 +1,25 @@
+import shelve
+
+help(shelve)
+
+# *******************************<<
+
+# for obj in dir(shelve.Shelf):
+#     if obj[0] != '_':
+#         print(obj)
+
+# *******************************<<
+
+# import shelve
+#
+# print(dir())
+# print()
+# print(dir(shelve))
+
+# *******************************<<
+
+# print(dir())
+# print(dir(__builtins__))
+# for m in dir(__builtins__):
+#     print(m)
+
