269371b70de224ee93297c1cca60862c2732a8e7
Mariia Gorbachova <vg.shamking@gmail.com>
2022-09-25 00:39:36 +0200

    2022-09-25

---
+++ 170. WebBrowser Module/browser.py
@@ -0,0 +1,14 @@
+import webbrowser
+
+# help(webbrowser)
+#
+# for i in range(10):
+#     print(1, 2, 3, 4, 5, 6, 7, 8, 9, sep='; ')
+#     print()
+#
+# webbrowser.open("https://www.python.org/")
+#
+url = "https://www.python.org/"
+webbrowser.open(url, new=1)
+
+
