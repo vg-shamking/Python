5e231b6b22fdf01ff346e234bab060ca46d11e25
Mariia Gorbachova <vg.shamking@gmail.com>
2022-11-20 02:11:20 +0100

    2022-11-20

---
+++ 200. Importing and Underscores/import_test_2.py
@@ -0,0 +1,18 @@
+import bj_challenge_2
+
+# print(__name__)
+# bj_challenge_2.play()
+# print(bj_challenge_2.cards)
+
+# g = sorted(globals())
+# for x in g:
+#     print(x)
+
+# bj_challenge_2.deal_card(bj_challenge_2.dealer_card_frame)
+# bj_challenge_2.play()
+
+personal_details = ("Tim", 24, "Australia")
+name, _, country = personal_details
+print(name, country)
+print(_)
+
