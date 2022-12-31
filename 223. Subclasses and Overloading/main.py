85d74dfe6b71732fd55d0de16104947f88420b24
Mariia Gorbachova <vg.shamking@gmail.com>
2022-12-31 03:16:58 +0100

    2022-12-31

---
+++ 223. Subclasses and Overloading/main.py
@@ -0,0 +1,10 @@
+from enemy2 import Enemy, Troll
+
+ugly_troll = Troll()
+print("Ugly troll - {}".format(ugly_troll))
+
+another_troll = Troll("Ug", 18, 1)
+print("Another troll - {}".format(another_troll), end="\n")
+
+brother = Enemy("Urg", 23)
+print(brother)
