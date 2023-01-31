9bae0342637dfd87325a2ebcfedc4823340aac38
Mariia Gorbachova <vg.shamking@gmail.com>
2023-01-31 05:50:15 +0100

    2023-01-31

---
+++ 232. Test Code and Challenge/main.py
@@ -0,0 +1,45 @@
+from enemy11 import Enemy, Troll, Vampyre, VampyreKing
+
+dracula = VampyreKing("Dracula")
+print(dracula)
+dracula.take_damage(12)
+print(dracula)
+
+a = 3
+b = "tim"
+c = 1, 2, 3
+print(a)
+print(b)
+print(c)
+
+# ugly_troll = Troll("Pug")
+# print("Ugly troll - {}".format(ugly_troll))
+#
+# another_troll = Troll("Ug")
+# print("Another troll - {}".format(another_troll), end="\n")
+# another_troll.take_damage(18)
+# print(another_troll)
+#
+# brother = Troll("Urg")
+# print(brother)
+#
+# ugly_troll.grunt()
+# another_troll.grunt()
+# brother.grunt()
+#
+# vamp = Vampyre("Vlad")
+# print(vamp)
+# vamp.take_damage(5)
+# print(vamp)
+#
+# print("-" * 40)
+# another_troll.take_damage(30)
+# print(another_troll)
+#
+# # while vamp.alive:
+# #     vamp.take_damage(1)
+# #     # print(vamp)
+#
+# vamp._lives = 0
+# vamp._hit_points = 1
+# print(vamp)
