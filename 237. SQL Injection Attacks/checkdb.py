feb69e1c6e3b62ad4ed43428dd4dac6848175be3
Mariia Gorbachova <vg.shamking@gmail.com>
2023-03-13 01:48:31 +0100

    2023-03-13

--- 237. SQL Injection Attacks/checkdb.py
+++ 237. SQL Injection Attacks/checkdb.py
@@ -2,7 +2,7 @@ import sqlite3
 
 conn = sqlite3.connect("contacts.sqlite")
 
-for row in conn.execute("SELECT * FROM contacts"):
+for row in conn.execute("SELECT * FROM sqlite_master"):
     print(row)
 
 conn.close()
