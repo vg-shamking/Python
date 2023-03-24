b9f88dac618a084ce08b5b8fe5e95b20652863ad
Mariia Gorbachova <vg.shamking@gmail.com>
2023-03-24 01:35:09 +0100

    2023-03-24

--- 239. Exceptions/examples.py
+++ 239. Exceptions/examples.py
@@ -1,2 +1,15 @@
-x = 8 - 5
-y = x / 0
+def factorial(n):
+    # n! can also be defines as n * (n-1)!
+    """ calculates n! recursively """
+    if n <= 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+
+try:
+    print(factorial(900))
+except RecursionError:
+    print("This program calculate factorials that large")
+
+print("Program terminating")
