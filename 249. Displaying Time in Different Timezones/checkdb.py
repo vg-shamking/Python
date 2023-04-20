18b55c68b1a26ae6cc817d8aeedc7bce19549262
Mariia Gorbachova <vg.shamking@gmail.com>
2023-04-20 02:43:30 +0200

    2023-04-20

--- 249. Displaying Time in Different Timezones/checkdb.py
+++ 249. Displaying Time in Different Timezones/checkdb.py
@@ -1,10 +1,13 @@
 import sqlite3
+import pytz
 
 db = sqlite3.connect("accounts.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)
 
 for row in db.execute("SELECT * FROM history"):
+    utc_time = row[0]
+    local_time = pytz.utc.localize(utc_time).astimezone()
     # print(row)
     local_time = row[0]
-    print("{}\t{}".format(local_time, type(local_time)))
+    print("{}\t{}".format(utc_time, local_time))
 
 db.close()
