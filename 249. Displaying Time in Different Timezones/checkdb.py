974aefa7742fe7b3ed9281cb5eb1304ceb5417d1
Mariia Gorbachova <vg.shamking@gmail.com>
2023-04-20 02:32:46 +0200

    2023-04-20

--- 249. Displaying Time in Different Timezones/checkdb.py
+++ 249. Displaying Time in Different Timezones/checkdb.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-db = sqlite3.connect("accounts.sqlite")
+db = sqlite3.connect("accounts.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)
 
 for row in db.execute("SELECT * FROM history"):
     # print(row)
