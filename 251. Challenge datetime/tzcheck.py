f4fd6591b11f02d269c2f54c5572d69475db653e
Mariia Gorbachova <vg.shamking@gmail.com>
2023-05-02 01:43:38 +0200

    2023-05-02

--- 251. Challenge datetime/tzcheck.py
+++ 251. Challenge datetime/tzcheck.py
@@ -9,8 +9,11 @@ db = sqlite3.connect("accounts.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)
 #                       "history.account, history.amount FROM history ORDER BY history.time"):
 for row in db.execute("SELECT * FROM history"):
     utc_time = row[0]
-    picked_zone = row[3]
-    zone = pickle.loads(picked_zone)
+    # picked_zone = row[3]
+    # zone = pickle.loads(picked_zone)
+    zone = pytz.timezone("CET")
+    # zone = pytz.timezone("Australia/Adelaide")
+    # zone = pytz.timezone("ACDT")  # Error
     local_time = pytz.utc.localize(utc_time).astimezone(zone)
     print("{}\t{}\t{}".format(utc_time, local_time, local_time.tzinfo))
 
