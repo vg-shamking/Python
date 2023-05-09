b82962cc0f77eb283da54ce31f6d830c5813f7b2
Mariia Gorbachova <vg.shamking@gmail.com>
2023-05-10 01:31:21 +0200

    2023-05-10

--- 253. Simple Database Browser/jukebox.py
+++ 253. Simple Database Browser/jukebox.py
@@ -1,7 +1,14 @@
 import sqlite3
 
+try:
+    import tkinter
+except ImportError:  # python 2
+    import Tkinter as tkinter
+
 conn = sqlite3.connect('music.sqlite')
 
-for x in conn.execute("SELECT albums.name FROM albums WHERE albums.artist = ? ORDER BY albums.name", (196,)):
-    print(x)
-    
+mainWindow = tkinter.Tk()
+mainWindow.title('Music DB Browser')
+mainWindow.geometry('1024x768')
+
+
