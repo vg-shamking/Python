b9790e10ec8ea77389f51dcc99f07c9d6a701a3d
Mariia Gorbachova <vg.shamking@gmail.com>
2024-02-02 08:37:54 +0100

    2024-02-02

---
+++ 001. Printing/printing.py
@@ -0,0 +1,10 @@
+print('Hello, World!')
+
+#  Staring with ' must be finished
+#  Can be used ' or "
+print(1 + 2)
+print(0 + 0)
+print(7 / 2)
+print(2 * 4)
+print("The End")
+print("The End", "or is it?", "keep watching to learn more about Python", 3)
